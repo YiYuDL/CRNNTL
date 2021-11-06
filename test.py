080ee8fb0f366f0947d4fd3bf1532eae80d0f476
Yi Yu <58670828+YiYuDL@users.noreply.github.com>
2021-11-06 15:08:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -8,14 +8,17 @@ from CRNNTL.preprocessing import data_pre
 from CRNNTL.train import CNN_CRNN_train
 from CRNNTL.baseline import SVM
 from absl import app
+import pandas as pd
 
 def main(argv):
-    X_fgfr1, DF_fgfr1 = data_pre('fgfr1')
+    fgfr1_df = pd.read_csv('datasets/fgfr1.csv', index_col=0)
+    X_fgfr1, DF_fgfr1 = data_pre(fgfr1_df)
     CNN_result,CRNN_result = CNN_CRNN_train(X_fgfr1, DF_fgfr1)
-    SVM_result = SVM('fgfr1')
+    SVM_result = SVM(fgfr1_df)
     print('CNN','fgfr1',"%0.2f"%(CNN_result))
     print('CRNN','fgfr1',"%0.2f"%(CRNN_result))
     print('baseline SVM',"%0.2f"%(SVM_result))
     
 if __name__ == "__main__":
     app.run(main)
+    
